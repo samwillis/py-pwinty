4afa72d3907eafb40e98f0282983df7eafafe19c
Sam Willis <sam.willis@gmail.com>
2014-01-11 16:54:56 +0000

    Chaged package layout to work with PyPi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
     name = 'pwinty',
-    scripts = ['pwinty.py'],
-    version = '0.1',
+    packages = ['pwinty'],
+    version = '0.2',
     description = 'Python client for the Pwinty photo printing API',
     author = 'Sam Willis',
     author_email = 'sam.willis@gmail.com',
     url = 'https://github.com/samwillis/py-pwinty',
-    download_url = 'https://github.com/samwillis/py-pwinty/tarball/0.1',
+    download_url = 'https://github.com/samwillis/py-pwinty/tarball/0.2',
     keywords = ['printing'],
     classifiers = [],
 )
